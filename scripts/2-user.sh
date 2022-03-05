dd1c385b1966e6aec12ed271b1e57ca8972a5169
Archi <51773524+mariotani25@users.noreply.github.com>
2022-03-05 12:44:42 +0700

    Update 2-user.sh

--- scripts/2-user.sh
+++ scripts/2-user.sh
@@ -54,9 +54,9 @@ if [[ $INSTALL_TYPE == "FULL" ]]; then
   if [[ $DESKTOP_ENV == "kde" ]]; then
     cp -r ~/ArchInstall/configs/.config/* ~/.config/
     # pip install konsave
-    konsave -i ~/ArchInstall/configs/kde.knsv
+    python -m konsave -i ~/ArchInstall/configs/kde.knsv
     sleep 1
-    konsave -a kade
+    python -m konsave -a kde
   elif [[ $DESKTOP_ENV == "openbox" ]]; then
     cd ~
     git clone https://github.com/stojshic/dotfiles-openbox
