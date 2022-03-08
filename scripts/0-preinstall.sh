8d20ae7f73e9d09546317a8cde5ee3a31f6cde41
Archi <51773524+mariotani25@users.noreply.github.com>
2022-03-08 14:21:22 +0700

    Update 0-preinstall.sh

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -22,7 +22,8 @@ echo -ne "
                     Setting up $iso mirrors for faster downloads
 -------------------------------------------------------------------------
 "
-reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+# reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+reflector -a 48 -c $iso -f 20 -l 30 --number 10 --save /etc/pacman.d/mirrorlist
 mkdir /mnt &>/dev/null # Hiding error message if any
 echo -ne "
 -------------------------------------------------------------------------
